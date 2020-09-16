2ba51cf6cc6bcc6977938b54380fbe738f4be457
digvijay571 <digvjay18012001@gmail.com>
2020-09-16 04:09:53 -0700

    refactor

--- index1.js
+++ index1.js
@@ -1,73 +1,31 @@
 
 
 $(document).ready(function () {
+  $(".i1").on({
+    mouseenter: function () {$(".i1>a")[0].style.color = "red";},
+    mouseleave: function () {$(".i1>a")[0].style.color = "rgb(142,116,116)";},
+  });
   $(".i2").on({
-    mouseenter: function () {
-      $(".dropdown_1").removeClass("hide").animate({
-        opacity: 1,
-        // top: "100%",
-      });
-      $(".i2>a")[0].style.color = "red";
-    },
-    mouseleave: function () {
-      
-      $(".dropdown_1").addClass("hide").animate({
-        opacity: 0.1,
-        // top: "15%",
-      });
-      $(".i2>a")[0].style.color = "rgb(142,116,116)";
-    },
+    mouseenter: function () {$(".i2>a")[0].style.color = "red";},
+    mouseleave: function () { $(".i2>a")[0].style.color = "rgb(142,116,116)";},
   });
 
   $(".i3").on({
-    mouseenter: function () {
-      $(".dropdown_2").removeClass("hide").animate({
-        opacity: 1,
-        // top: "0",
-      });
-      $(".i3>a")[0].style.color = "red";
-    },
-    mouseleave: function () {
-      $(".dropdown_2").addClass("hide").animate({
-        opacity: 0.1,
-        // top: "15%",
-      });
-      $(".i3>a")[0].style.color = "rgb(142,116,116)";
-    },
+    mouseenter: function () {$(".i3>a")[0].style.color = "red";},
+    mouseleave: function () {$(".i3>a")[0].style.color = "rgb(142,116,116)";},
   });
 
   $(".i4").on({
-    mouseenter: function () {
-      $(".dropdown_3").removeClass("hide").animate({
-        opacity: 1,
-        // top: "0",
-      });
-      $(".i4>a")[0].style.color = "red";
-    },
-    mouseleave: function () {
-      $(".dropdown_3").addClass("hide").animate({
-        opacity: 0.1,
-        // top: "15%",
-      });
-      $(".i4>a")[0].style.color = "rgb(142,116,116)";
-    },
+    mouseenter: function () {$(".i4>a")[0].style.color = "red";},
+    mouseleave: function () {$(".i4>a")[0].style.color = "rgb(142,116,116)";},
   });
 
-  $(".i1").on({
-    mouseenter: function () {
-      $(".i1>a")[0].style.color = "red";
-    },
-    mouseleave: function () {
-      $(".i1>a")[0].style.color = "rgb(142,116,116)";
-    },
-  });
 
   $(".i5").on({
     mouseenter: function () { $(".i5>a")[0].style.color = "red";},
     mouseleave: function () {$(".i5>a")[0].style.color = "rgb(142,116,116)";},
   });
-
-  $(".hamburger>svg").click(function () {
+  $("img[src='hamburger.svg']").click(function () {
     $(".sub_menu").slideToggle();
   });
   $(".sub1").click(function () {
